4955145faedea582627b08c102ea5234b6e9ba54
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2021-11-22 10:13:22 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace popUpMenu_1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+
+            //button1.Click += delegate
+            //{
+            //    PopupMenu
+            //    menu = new PopupMenu(this, button1);
+            //    menu.Inflate(Resource.Menu.popMenu);
+            //    menu.Show();
+            //};
+
+            button1.Click += (s, arg) =>
+            {
+                PopupMenu menu = new PopupMenu(this, button1);
+                menu.Inflate(Resource.Menu.popMenu);
+
+                menu.MenuItemClick += (s1, arg1) =>
+                {
+                    Toast.MakeText(this, arg1.Item.TitleFormatted + "  selected"  , ToastLength.Short).Show();
+                };
+
+                menu.DismissEvent += (s2, arg2) =>
+                {
+                  //  Console.WriteLine("menu dismissed");
+                    Toast.MakeText(this, "menu dismissed", ToastLength.Long).Show();
+                };
+                menu.Show();
+            };
+
+            Switch s = FindViewById<Switch>(Resource.Id.do_switch);
+
+            s.CheckedChange += delegate (object sender, CompoundButton.CheckedChangeEventArgs e) {
+
+                var toast = Toast.MakeText(this, "Your answer is " + (e.IsChecked ? "correct" : "incorrect"),                                    ToastLength.Short);
+                toast.Show();
+
+               // Toast.MakeText(this, "Your answer is " + (e.IsChecked ? "correct" : "incorrect"), ToastLength.Short).Show();
+
+                //if (e.IsChecked)
+                //{
+                //    Toast.MakeText(this, "correct", ToastLength.Short).Show();
+                //}
+                //else
+                //{
+                //    Toast.MakeText(this, "incorrect", ToastLength.Short).Show();
+                //}
+            };
+
+
+
+            var web = FindViewById<EditText>(Resource.Id.web);
+            var display = FindViewById<Button>(Resource.Id.display);
+
+            display.Click += delegate
+            {
+                Intent i = new Intent(this, typeof(WebViewActivity));
+                i.PutExtra("web", web.Text);
+                StartActivity(i);
+
+            };
+        }
+    }
+}
---
+++ WebViewActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Webkit;
+
+namespace popUpMenu_1
+{
+    [Activity(Label = "WebViewActivity")]
+    public class WebViewActivity : Activity
+    {
+        WebView web_view;
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_webview);
+
+            string web = Intent.GetStringExtra("web");
+
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new HelloWebViewClient());
+            web_view.LoadUrl(web);
+        }
+        public class HelloWebViewClient : WebViewClient
+        {
+            // For API level 24 and later
+            public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
+            {
+                view.LoadUrl(request.Url.ToString());
+                return false;
+            }
+        }
+
+        //public class HelloWebViewClient : WebViewClient
+        //{
+        //    public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        //    {
+        //        view.LoadUrl(url);
+        //        return false;
+        //    }
+        //}
+
+    }
+}
